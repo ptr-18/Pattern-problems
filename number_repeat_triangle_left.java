a8ec7e93315edd15f4fc88d88d510e11f67a58ca
ptr-18 <146318223+ptr-18@users.noreply.github.com>
2026-06-01 17:33:13 +0530

    Add files via upload

---
+++ number_repeat_triangle_left.java
@@ -0,0 +1,13 @@
+public class number_repeat_triangle_left {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print(i);
+            }
+            System.out.println("");
+        }
+
+    }
+
+}
